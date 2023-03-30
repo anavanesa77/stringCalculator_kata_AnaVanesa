cb42c771c372f02f2367bf256a64ade1d99efa27
anavanesa77 <anav.olivares@gmail.com>
2023-03-30 16:44:02 +0100

    :green_square: print_exception_if_there_are_negative_numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,32 +1,58 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringCalculator {
-    public static int Add(String numbers) {
+    public static int Add(String numbers) throws NegativeNumberException {
         if (numbers.equals("")) {
             return 0;
         }
         if (numbers.startsWith("/")) {
             var delimiter = numbers.substring(2, 3);
             var sumOfNumbers = extractNumbersWithDelimiter(numbers, delimiter);
+            var negativeNumbers = extractListNegativeNumber(sumOfNumbers);
+            failIfThereAreNegativeNumbers(negativeNumbers);
             return toAddNumbers(sumOfNumbers);
         }
-        var sumOfNumbers = extractNumbers(numbers);
-        return  toAddNumbers(sumOfNumbers);
+        var sumOfNumbers = extractNumbersWithoutDelimiter(numbers);
+        var negativeNumbers = extractListNegativeNumber(sumOfNumbers);
+        failIfThereAreNegativeNumbers(negativeNumbers);
+        return toAddNumbers(sumOfNumbers);
     }
-
-    private static List<Integer> extractNumbers(String text) {
-        var textOperationNumbers = List.of(text.replaceAll("\n",",").split(","));
+    private static List<Integer> extractNumbersWithoutDelimiter(String text) {
+        var textOperationNumbers = List.of(text.replaceAll("\n", ",").split(","));
         return textOperationNumbers.stream().map(Integer::valueOf).toList();
     }
-
     private static Integer toAddNumbers(List<Integer> operationNumbers) {
         return operationNumbers.stream().reduce(0, Integer::sum);
     }
-
     private static List<Integer> extractNumbersWithDelimiter(String text, String delimiter) {
         var textNumbers = List.of(text.substring(4).split(delimiter));
         return textNumbers.stream().map(Integer::valueOf).toList();
     }
+    private static List<Integer> extractListNegativeNumber(List<Integer> operationNumbers) throws NegativeNumberException {
+        List negativeNumbers = new ArrayList();
+        for (int numbers : operationNumbers) {
+            if (numbers < 0) {
+                negativeNumbers.add(numbers);
+            }
+        }
+        return negativeNumbers;
+    }
+    private static String createErrorMessage(List<Integer> negativeNumbers) {
+        var errorMessage = "error: negatives not allowed";
+        for (int number : negativeNumbers) {
+            if (number < 0) {
+                errorMessage = errorMessage + (" ") + number;
+            }
+        }
+        return errorMessage;
 
+    }
+    private static void failIfThereAreNegativeNumbers(List<Integer> negativeNumbers) throws NegativeNumberException {
+        if (!negativeNumbers.isEmpty()) {
+            var errorMessage = createErrorMessage(negativeNumbers);
+            throw new NegativeNumberException(errorMessage);
+        }
+    }
 }
 
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -6,33 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorShould {
     @Test
-    void print_zero_when_receiving_an_empty_character() {
+    void print_zero_when_receiving_an_empty_character() throws NegativeNumberException {
         String character = "";
         int mathOperation = StringCalculator.Add(character);
         Assertions.assertEquals(0,  mathOperation);
     }
 
     @Test
-    void print_the_same_number() {
+    void print_the_same_number() throws NegativeNumberException {
         String number = "1";
         int mathOperation  = StringCalculator.Add(number);
         Assertions.assertEquals(1,mathOperation);
     }
 
     @Test
-    void print_sum_of_the_numbers() {
+    void print_sum_of_the_numbers() throws NegativeNumberException {
         String numbers = "1,2,3,4";
         int mathOperation = StringCalculator.Add(numbers);
         Assertions.assertEquals(10,mathOperation);
     }
     @Test
-    void print_sum_of_numbers_without_line_break() {
+    void print_sum_of_numbers_without_line_break() throws NegativeNumberException {
         var number = "1\n2\n3";
         int mathOperation = StringCalculator.Add(number);
         Assertions.assertEquals(6, mathOperation);
     }
     @Test
-    void print_sum_of_numbers_with_any_delimiter() {
+    void print_sum_of_numbers_with_any_delimiter() throws NegativeNumberException {
         var number = "//;\n1;2";
         int mathOperation = StringCalculator.Add(number);
         Assertions.assertEquals(3,mathOperation);
@@ -44,5 +44,4 @@ class StringCalculatorShould {
                 StringCalculator.Add("//;\n-1;-2"));
         Assertions.assertEquals(errorMessage, capturedExcepcion.getMessage());
     }
-
 }
