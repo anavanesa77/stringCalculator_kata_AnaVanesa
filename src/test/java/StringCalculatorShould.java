b0a4459ba51bfa84c73845f1147e9064da1fae65
anavanesa77 <anav.olivares@gmail.com>
2023-03-30 13:53:53 +0100

    :red_square:  print_sum_of_numbers_without_line_break

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -7,21 +7,28 @@ class StringCalculatorShould {
     void print_zero_when_receiving_an_empty_character() {
         String character = "";
         int mathOperation = StringCalculator.Add(character);
-        Assertions.assertEquals(0,  mathOperation);
+        Assertions.assertEquals(0, mathOperation);
     }
 
     @Test
     void print_the_same_number() {
         String number = "1";
-        int mathOperation  = StringCalculator.Add(number);
-        Assertions.assertEquals(1,mathOperation);
+        int mathOperation = StringCalculator.Add(number);
+        Assertions.assertEquals(1, mathOperation);
     }
 
     @Test
     void print_sum_of_the_numbers() {
         String numbers = "1,2,3,4";
         int mathOperation = StringCalculator.Add(numbers);
-        Assertions.assertEquals(10,mathOperation);
+        Assertions.assertEquals(10, mathOperation);
+    }
+
+    @Test
+    void print_sum_of_numbers_without_line_break() {
+        var number = "1\n2\n3";
+        int mathOperation = StringCalculator.Add(number);
+        Assertions.assertEquals(6, mathOperation);
     }
 
 }
