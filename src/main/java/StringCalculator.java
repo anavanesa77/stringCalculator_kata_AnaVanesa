f02bc5d42fc5fb03bec358c73ee42515cf92172f
anavanesa77 <anav.olivares@gmail.com>
2023-03-30 14:25:02 +0100

    :green_square: print_sum_of_numbers_with_any_delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,6 +5,11 @@ public class StringCalculator {
         if (numbers.equals("")) {
             return 0;
         }
+        if (numbers.startsWith("/")) {
+            var delimiter = numbers.substring(2, 3);
+            var sumOfNumbers = extractNumbersWithDelimiter(numbers, delimiter);
+            return toAddNumbers(sumOfNumbers);
+        }
         var sumOfNumbers = extractNumbers(numbers);
         return  toAddNumbers(sumOfNumbers);
     }
@@ -16,7 +21,11 @@ public class StringCalculator {
 
     private static Integer toAddNumbers(List<Integer> operationNumbers) {
         return operationNumbers.stream().reduce(0, Integer::sum);
+    }
 
+    private static List<Integer> extractNumbersWithDelimiter(String text, String delimiter) {
+        var textNumbers = List.of(text.substring(4).split(delimiter));
+        return textNumbers.stream().map(Integer::valueOf).toList();
     }
 
 }
