e9d015c6a179f0ee7b2b9b7f6112652543131dd7
anavanesa77 <anav.olivares@gmail.com>
2023-03-30 13:38:41 +0100

    :green_square: print_sum_of_the_numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,20 @@
+import java.util.List;
+
 public class StringCalculator {
     public static int Add(String numbers) {
         if (numbers.equals("")) {
             return 0;
         }
-        if (numbers.equals("1")) {
-            return 1;
-        }
-        return 0;
+        var sumOfNumbers = extractNumbers(numbers);
+        return  toAddNumbers(sumOfNumbers);
+    }
+
+    private static List<Integer> extractNumbers(String numbers) {
+        var textOperationNumbers = List.of(numbers.split(","));
+        return textOperationNumbers.stream().map(Integer::valueOf).toList();
+    }
+
+    private static Integer toAddNumbers(List<Integer> operationNumbers) {
+        return operationNumbers.stream().reduce(0, Integer::sum);
     }
 }
